8e6b231bd6812d0a20aee173332232166b541a6c
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2023-06-18 23:45:06 +0530

    Create e sum of two numbers.c

---
+++ Tutorials/Tutorial_01/e sum of two numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+  int x,y,sum;
+  printf("Enter First Number: ");
+  scanf("%d",&x);
+  printf("Enter Second number: ");
+  scanf("%d",&y);
+  sum=x+y;
+  printf("%d",sum);
+}
