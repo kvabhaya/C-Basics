e588f6a72edb68d02181efb8b3c7c1b4a9dcd5c9
Kokila Abhayasundara <94110759+kvabhaya@users.noreply.github.com>
2023-07-09 06:22:05 +0530

    Update function_intro_part_1_1.c

--- function_intro_part_1_1.c
+++ function_intro_part_1_1.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
+void sub()
+{
+  int a,b;
+  printf("\nEnter 2 Numbers : ");
+  scanf("%d %d",&a,&b);
+  printf("\nSub is %d",a-b);
+}
+
 void sum()
 {
   int m,n;
-  printf("Enter two Numbers : ");
+  printf("\nEnter two Numbers : ");
   scanf("%d %d",&m,&n);
   printf("\nSum is %d",m+n);
+  sub();
 }
 void main()
 {
